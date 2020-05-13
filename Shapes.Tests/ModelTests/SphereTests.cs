5e6dbef388a7d79a2d3722b1a7af616acbbb9d37
DJ Zevenbergen <djzevenbergen@gmail.com>
2020-05-13 10:40:52 -0600

    fix rounding

--- Shapes.Tests/ModelTests/SphereTests.cs
+++ Shapes.Tests/ModelTests/SphereTests.cs
@@ -12,7 +12,7 @@ namespace Shapes.Tests
       Circle circle = new Circle(2);
       Sphere sphere = new Sphere(circle);
       double result = sphere.GetVolume();
-      Assert.AreEqual(result, 33.51);
+      Assert.AreEqual(result, 33.49);
     }
   }
 }
